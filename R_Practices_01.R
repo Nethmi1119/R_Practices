9782c3a623b49fcb3a2e95765ed80e9609777963
Nethmi1119 <apsaranethmi2001@gmail.com>
2025-09-17 09:24:30 +0530

    Add files via upload

---
+++ R_Practices_01.R
@@ -0,0 +1,26 @@
+#R practise 01
+#Arithmetic Operators
+
+6+5
+10/3
+2^3
+9+8/3
+2*8/4^2
+
+#Functions
+
+log(10)
+exp(1)
+log(exp(1))
+log10(2)
+log(100,base = 10)
+log2(4)
+sqrt(16)
+pi
+sin(pi/2)
+cos(pi/3)
+tan(pi/4)
+round(2.3)
+floor(3.3)
+ceiling(5.7)
+
